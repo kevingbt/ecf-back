932298c51c8979ae4c3efac6334a49ae007fad22
Kevin Guibert <kevin.gbt2805@gmail.com>
2026-05-20 11:12:31 +0200

    ajout role admin et bibliothecaire

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -66,8 +66,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        // guarantee every user at least has ROLE_BIBLIOTHECAIRE
+        $roles[] = 'ROLE_BIBLIOTHECAIRE';
+
 
         return array_unique($roles);
     }
